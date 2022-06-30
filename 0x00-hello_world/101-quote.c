4109ea3427a9ecf032d9a6f0ad0eb9add9f0b911
Ayanfisco <Ayanfiscoss@gmail.com>
2022-06-30 13:04:04 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *
+ * main - main function
+ *
+ * return - end function
+ */
+int main(void)
+{
+	char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19'";
+	fprintf(stderr, "%s\n", n);
+	return (1);
+}
