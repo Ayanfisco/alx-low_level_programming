578733a01805e72ca4b50ca419b4c83d8f948935
Ayanfisco <Ayanfiscoss@gmail.com>
2022-07-01 16:15:28 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: end of the program
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; ++n)
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+	
+		}
+	}
+	putchar(10);
+	return (0);
+}
