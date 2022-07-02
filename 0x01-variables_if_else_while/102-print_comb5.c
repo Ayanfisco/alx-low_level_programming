2220881424234008c7ff40df8054150dfc146e81
Ayanfisco <Ayanfiscoss@gmail.com>
2022-07-01 17:58:04 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,35 +12,35 @@ int main(void)
 	while (a <= 9)
 	{
 		while (b <= 9)
+	{
+		while (c <= 9)
 		{
-			while (c <= 9)
+			while (d <= 9)
 			{
-				while (d <= 9)
+				if ((c * 10) + d > (a * 10) + b)
 				{
-					if ((c * 10) + d > (a * 10) + b)
+					if (coma == 0)
 					{
-						if (coma == 0)
-						{
-							putchar(',');
-							putchar(32);
-						}
-						putchar(a + '0');
-						putchar(b + '0');
+						putchar(',');
 						putchar(32);
-						putchar(c + '0');
-						putchar(d + '0');
-						coma = 0;
 					}
-					++d;
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(32);
+					putchar(c + '0');
+					putchar(d + '0');
+					coma = 0;
 				}
-				++c;
-				d = 0;
+				++d;
 			}
-			++b;
-			c = 0;
+			++c;
+			d = 0;
 		}
-		++a;
-		b = 0;
+		++b;
+		c = 0;
+	}
+	++a;
+	b = 0;
 	}
 
 	putchar(10);
