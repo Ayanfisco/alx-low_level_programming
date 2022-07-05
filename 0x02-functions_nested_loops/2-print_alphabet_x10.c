eff3969b841836b88422577027cfad128d667ece
Ayanfisco <Ayanfiscoss@gmail.com>
2022-07-05 09:15:21 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_X10 - prints alphabet X10
+ */
+void print_alphabet_x10(void)
+{
+	char n = '1';
+	char a;
+
+	while (n <= '10')
+	{
+		for (a = 'a'; a <= 'z'; ++a)
+		{
+			_putchar(a);
+		}
+		_putchar(10);
+		++n;
+	}
+}
