6d25739d4566fc1d8a3db5d28f8015ca4255749a
Ayanfisco <Ayanfiscoss@gmail.com>
2022-07-05 13:44:13 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- *
  * print_sign - function
  *
- * @n: The character to evaluate
+ * @c: The character to evaluate
  *
  * Return: Get 1 if c is positive,
  * get 0 if it's 0, and
