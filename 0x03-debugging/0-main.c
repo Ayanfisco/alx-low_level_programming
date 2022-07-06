55e79ae7af17d1409becf967343839cf4da95298
Ayanfisco <Ayanfiscoss@gmail.com>
2022-07-06 05:43:47 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - tests function that prints if integer is negative or positive
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 98;
+	positive_or_negative(n);
+	return (0);
+}
