ca998129262fe407318a9be443accc9423ebdaef
Ayanfisco <Ayanfiscoss@gmail.com>
2022-07-07 16:50:43 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * more_numbers - function that checks for uppercase character
- * Return: 0
  */
 void more_numbers(void)
 {
@@ -9,17 +8,14 @@ void more_numbers(void)
 	int a = 0;
 	int b = 0;
 
-
 	while (b <= 9)
 	{
 		while (a <= 14)
 		{
 			if (a > 9)
-				_putchar(a / 10 + '0');
+			_putchar(a / 10 + '0');
 			_putchar(a % 10 + '0');
 			a++;
-
-		
 		}
 		_putchar('\n');
 		b++;
